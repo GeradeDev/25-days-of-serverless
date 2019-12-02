6e26aeb4701bf5c8fddea56be7011637c3214968
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2019-12-02 13:51:52 +0200

    -added dreidel function app

---
+++ week-1/challenge-1/Dreidel/GetDreidel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Dreidel
+{
+    public static class GetDreidel
+    {
+        private static string[] symbols = new[] { "נ", "ג", "ה", "ש" };
+        private static string[] names = new[] { "Nun", "Gimmel", "Hay", "Shin" };
+
+        [FunctionName("getDreidel")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            int rndNum = new Random().Next(symbols.Length - 1);
+
+            return (ActionResult)new OkObjectResult(symbols[rndNum]);
+        }
+    }
+}
